95f696ee33af8875084f2503386a42ffe92be2ea
lkmdev01 <lkmdev01@gmail.com>
2024-11-30 20:02:16 -0300

    alterações de design

--- app/src/components/MyHeader.vue
+++ app/src/components/MyHeader.vue
@@ -2,12 +2,12 @@
   <div class="bg-white">
     <header class="inset-x-0 top-0 z-50 bg-white">
       <nav
-        class="flex items-center justify-between p-6 lg:px-8"
+        class="flex items-center justify-between py-2 px-6 lg:px-8"
         aria-label="Global"
       >
         <div class="flex lg:flex-1">
           <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
+            <img class="h-16 w-auto" src="/logo.png" alt="" />
           </a>
         </div>
         <div class="flex lg:hidden">
@@ -139,5 +139,8 @@ const navigateToSection = (href: string) => {
 </script>
 
 <style scoped>
-/* Estilos adicionais, se necessário */
+header{
+  max-width: 1400px;
+  margin: 0 auto;
+}
 </style>
